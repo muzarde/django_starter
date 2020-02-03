ed7f6b29833a4f3ee5f17c6aa95c3e12fde5173f
Hugo Jérémie Roland Musard <hugo.musard@epfl.ch>
2020-02-03 10:09:08 +0100

    d

--- Projet/Projet/settings.py
+++ Projet/Projet/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tsy#*&$5rxw=q$xc8@42pr0_&hm6)q4d5h_n+8c5n0920ja479'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True 
 
 ALLOWED_HOSTS = []
 
