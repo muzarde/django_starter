44fa327f2878a2df17aac57d16184ee490a3c8e8
Hugo Jérémie Roland Musard <hugo.musard@epfl.ch>
2020-02-06 14:22:32 +0100

    d

--- Projet/Projet/settings.py
+++ Projet/Projet/settings.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from os.path import abspath, dirname, join
+from dotenv import load_dotenv
+
+DOTENV_PATH = join(dirname(dirname(dirname(abspath(__file__)))), '.env')
+load_dotenv(dotenv_path=DOTENV_PATH, verbose=True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
